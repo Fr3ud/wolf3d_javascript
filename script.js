348f8eed59ecba2e3eba2b427c067bc84bc159ac
Fr3ud <oo.pavelko@gmail.com>
2018-07-06 22:44:58 +0300

    Adds minimap

---
+++ script.js
@@ -0,0 +1,148 @@
+(function() {
+
+     const map = [
+         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+         [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+     ];
+
+     const minimap = document.getElementById('minimap');
+     const objects = document.getElementById('objects');
+     const width = map[0].length;
+     const height = map.length;
+     const scale = 8;
+     const player = {
+         x: 4,
+         y: 2,
+         direction: 0,
+         rotation: 0,
+         speed: 0,
+         moveSpeed: 0.15,
+         rotationSpeed: 0.15
+     };
+
+     function keys() {
+         window.addEventListener('keydown', (e) => {
+             console.log(e);
+             switch (e.key) {
+                 case 'ArrowUp':
+                     player.speed = 1;
+                     console.log(player.x, player.y);
+                     break;
+                 case 'ArrowDown':
+                     player.speed = -1;
+                     console.log(player.x, player.y);
+                     break;
+                 case 'ArrowRight':
+                     player.direction = 1;
+                     console.log(player.x, player.y);
+                     break;
+                 case 'ArrowLeft':
+                     player.direction = -1;
+                     console.log(player.x, player.y);
+                     break;
+             }
+         }, true);
+
+         window.addEventListener('keyup', (e) => {
+             switch (e.key) {
+                 case 'ArrowUp':
+                 case 'ArrowDown':
+                     console.log('up');
+                     player.speed = 0;
+                     break;
+                 case 'ArrowRight':
+                 case 'ArrowLeft':
+                     player.direction = 0;
+                     break;
+             }
+         }, true);
+     }
+
+    function move() {
+        const moveStep = player.speed * player.moveSpeed;
+
+        player.rotation += player.direction * player.rotationSpeed;
+        player.x = player.x + Math.cos(player.rotation) * moveStep;
+        player.y = player.y + Math.sin(player.rotation) * moveStep;
+    }
+
+    function updateMinimap() {
+        const objectsCtx = objects.getContext('2d');
+
+        objectsCtx.clearRect(0, 0, minimap.width, minimap.height);
+        objectsCtx.fillRect(player.x * scale, player.y * scale, scale, scale);
+        objectsCtx.beginPath();
+        objectsCtx.moveTo(player.x * scale + 4, player.y * scale + 4);
+        objectsCtx.lineTo(
+            (player.x + Math.cos(player.rotation) * 4) * scale,
+            (player.y + Math.sin(player.rotation) * 4) * scale
+        );
+        objectsCtx.closePath();
+        objectsCtx.stroke();
+
+    }
+
+    function drawMinimap() {
+        const ctx = minimap.getContext('2d');
+
+        minimap.width = width * scale;
+        minimap.height = height * scale;
+        objects.width = width * scale;
+        objects.height = height * scale;
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                let block = map[x][y];
+
+                if (block) {
+                    ctx.fillStyle = '#bebebe';
+                    ctx.fillRect(x * scale, y * scale, scale, scale);
+                }
+            }
+        }
+        updateMinimap();
+    }
+
+    function loop() {
+        move();
+        updateMinimap();
+        setTimeout(loop, 33);
+    }
+
+    function init() {
+        keys();
+        drawMinimap();
+        loop();
+    }
+
+     init();
+ })();
